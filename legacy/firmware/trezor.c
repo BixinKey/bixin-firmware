25dd2f696d607403ec5e0f71f02671a8c5c7dd6d
kedoubitcoin <kedoubitcoin@126.com>
2020-10-23 12:09:16 +0800

    legacy:add charge display.

--- legacy/firmware/trezor.c
+++ legacy/firmware/trezor.c
@@ -130,6 +130,7 @@ int main(void) {
   ble_reset();
 #if !EMULATOR
   register_timer("button", timer1s / 2, buttonsTimer);
+  register_timer("chag_dis", timer1s, chagdisTimer);
 #endif
   __stack_chk_guard = random32();  // this supports compiler provided
                                    // unpredictable stack protection checks
